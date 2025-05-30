e5d112d785aa7e11da27b20648d4dad6fd2fcc75
shadow0708-0 <sathwik07084@gmail.com>
2025-05-30 19:46:22 +0530

    Update config.js

--- src/components/config.js
+++ src/components/config.js
@@ -6,7 +6,7 @@ const config = {
   */
 
   // 🔐 Passcode Page
-  correctPasscode: "0440", // Change this to any 4-digit code (the passcode for access)
+  correctPasscode: "0550", // Change this to any 4-digit code (the passcode for access)
   passcodeTitle: "Enter Passcode", // Title displayed on the passcode entry page
   successMessage: "Yayy!! :)", // Message shown upon successful passcode entry
   redirectMessage: "Redirecting...", // Message displayed while redirecting after success
